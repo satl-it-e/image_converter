af666a634424035305fb185d1af4bb2663df664a
LAnastasiia <lazorenko@ucu.edu.ua>
2019-10-01 23:55:57 +0300

    v01

--- v-0_image-converter.sh
+++ v-0_image-converter.sh
@@ -1,63 +1,63 @@
 #!/bin/bash
 
+# Set the "strict mode"
+# -e => Exit if any command has a non-zero exit status.
+# -u => Exit on references to undefined variables.
+# -o pipeline => Return err-code of the first-failed command as an err-code of a whole pipeline.
+# -o errexit => Exit script in case of err.
+set -euo pipefail errexit
+# Set Internal Field Separator for filenames.
+IFS=$'\n\t'
+
 ID_LEN=${1:-8}
 INIT_ID=${2:-0}
 THIS_DIR=${3:-$PWD}
 
-echo ${THIS_DIR}
 
-# Make sure CURR_DIR ends with slash
+# Make sure THIS_DIR ends with slash
 if [ "${THIS_DIR: -1}" != "/" ]; then THIS_DIR="${THIS_DIR}/"; fi
 
-# Error checking.
+# Check THIS_DIR arg.
 if [ ! -d ${THIS_DIR} ]; then
   echo "Given path is not valid directory. Please check."
   exit 1;
 fi
 
+# Check ID-... args.
 if [ $ID_LEN -gt 50 -o $ID_LEN -eq 0 ]; then
   echo "Given id-length is not supported. Please use numbers in range [1, 50]."
   exit 1;
 fi
 
-echo dir: ${THIS_DIR}
-
 cd ${THIS_DIR}
-for f in *; do
-    echo $f;
-    [ -f "$f" ] || continue
-    if file "$f" |grep -qE 'image|bitmap'; then
-      # convert "$f" "${THIS_DIR}converted/${f%.*}.jpg";
+
+for f in ${THIS_DIR}*; do
+  [ -f "$f" ] || continue
+
+    if [ $(file -ib ${f} | grep image) ]; then  # CHECK BITMAPS
+      echo stats: $(stat -c %y ${f})
       convert "$f" "${f%.*}.jpg";
     fi
+
 done
 
 
 
-# echo ${ID_LEN}
 
-# echo --${CURR_DIR}--
-# echo ${CURR_DIR:7}
 
-# CURR_DIR=${1:'/home/anastasiia/CS_UCU/'}
-#
-# cd ${CURR_DIR}
-# ls
+# ToDo
+# 1:
+# alternative img-type check
+  # if (echo "$set"  | fgrep -q "$WORD")
+  # built-in img-recognizing command
 
-# for f in $(ls -R)
-#   do
-#     echo "${f}"
-# done
+# 2:
+# check on bitmaps! else:
+# if [ $("$f" | grep -qE 'image|bitmap') ]; then
+# if [[ $(file -b ${f} | awk '{print $1}') =~ ^(PNG|JPG|GIF|BMP)$ ]]; then
+#   echo ---${f};
+# fi
+# SUPPORTED_IMG_EXTS=[]
 
-# function_recursive_ls () {
-#   if [ $( ls -d */ |  wc -l) -eq 0 ]; then
-#     cd ..
-#   else
-#     for sub_dir in $(ls -d */)
-#       do
-#         function_recursive_ls ${sub_dir}
-#     done
-#   fi
-# }
-#
-# function_recursive_ls ${CURR_DIR}
+# 3:
+# convert "$f" "${THIS_DIR}converted/${f%.*}.jpg";
